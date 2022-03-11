90e5f9cf7fb7e83ddaef1338ae8a75f5da96a924
Guillermo Sequera <sequeracrespo@gmail.com>
2022-03-11 16:47:13 -0300

    project end

--- app.js
+++ app.js
@@ -44,6 +44,41 @@ const job = document.getElementById('job');
 const info = document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.nextBtn');
-const randomBtn = document.querySelector('.randomBtn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
+let currentItem = 1;
+
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson();
+});
+
+function showPerson() {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+};
+
+nextBtn.addEventListener('click', function() {
+  currentItem++;
+  if (currentItem > reviews.length -1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length -1;
+  }
+  showPerson();
+});
+
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  console.log(currentItem);
+  showPerson();
+});
